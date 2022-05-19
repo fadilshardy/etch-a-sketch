0f034110ad49bcf66437541b8b06384eb90f0103
fadilshardy <fadilshrdy@gmail.com>
2022-05-19 18:24:00 +0700

    add new sketch button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('#sketch-container');
 
 
-function createSketchGrid(rows = 16, cols = 16) {
+function createSketch(rows = 16, cols = 16) {
 
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -14,4 +14,17 @@ function createSketchGrid(rows = 16, cols = 16) {
     }
 }
 
-createSketchGrid();
+function createNewSketch() {
+
+    const rows = prompt('Enter new rows grid');
+    const cols = prompt('Enter new cols grid');
+
+    createSketch(rows, cols);
+}
+
+const newBtn = document.querySelector('#new-sketch');
+
+newBtn.addEventListener('click', createNewSketch);
+
+
+createSketch();
