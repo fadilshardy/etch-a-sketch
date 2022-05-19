484faf7088ecaae0b73896e3582116543e69acaa
fadilshardy <fadilshrdy@gmail.com>
2022-05-19 18:23:29 +0700

    edit grid style

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#sketch-container');
 
-const row = 16;
-const cell = 16;
 
-for (let i = 0; i < row * cell; i++){
-    const div = document.createElement('div');
-    div.classList = 'grid-box';
-    container.appendChild(div);
+function createSketchGrid(rows = 16, cols = 16) {
+
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+
+    for (let i = 0; i < (rows * cols); i++){
+        const div = document.createElement('div');
+        div.classList = 'grid-box';
+        container.appendChild(div);
+    
+    }
 }
+
+createSketchGrid();
